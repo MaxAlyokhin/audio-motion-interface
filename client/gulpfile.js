359d39377b095607b28b090e2101c8a33b0c3c04
Max Alyokhin <55048182+MaxAlyokhin@users.noreply.github.com>
2023-03-11 00:24:59 +0300

    add babel

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -150,12 +150,12 @@ function js() {
           module: {
             rules: [
               {
-                // use: {
-                //   loader: 'babel-loader',
-                //   options: {
-                //     presets: ['@babel/preset-env'],
-                //   },
-                // },
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                  },
+                },
               },
             ],
           },
